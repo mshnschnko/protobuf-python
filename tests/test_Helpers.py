792f6cb2a117e6fc0599fe08e6567332b6e8909e
kmosin <k.mosin@nicetu.spb.ru>
2023-07-13 22:54:07 +0300

    Add tests

---
+++ tests/test_Helpers.py
@@ -0,0 +1,59 @@
+from protobuf_parser.helpers import parseDelimited
+from protobuf.message_pb2 import *
+
+from google.protobuf.internal.encoder import _VarintBytes
+
+import unittest
+
+class ParseTest(unittest.TestCase):
+    def test_default(self):
+        message = WrapperMessage(
+            request_for_fast_response=RequestForFastResponse()
+        )
+
+        data = _VarintBytes(message.ByteSize()) + message.SerializeToString()
+
+        message, bytesConsumed = parseDelimited(data, WrapperMessage)
+        
+        self.assertTrue(message.HasField('request_for_fast_response'))
+        self.assertEqual(bytesConsumed, len(data))
+
+    def test_nullData(self):
+        message, bytesConsumed = parseDelimited(None, WrapperMessage)
+        
+        self.assertIsNone(message)
+        self.assertEqual(bytesConsumed, 0)
+
+    def test_emptyData(self):
+        message, bytesConsumed = parseDelimited(b'', WrapperMessage)
+        
+        self.assertIsNone(message)
+        self.assertEqual(bytesConsumed, 0)
+
+    def test_wrongData(self):
+        data = b'\x05wrong'
+        message, bytesConsumed = parseDelimited(data, WrapperMessage)
+        
+        self.assertIsNone(message)
+        self.assertEqual(bytesConsumed, len(data))
+
+        data = b'message'
+        message, bytesConsumed = parseDelimited(data, WrapperMessage)
+        
+        self.assertIsNone(message)
+        self.assertEqual(bytesConsumed, 0)
+
+    def test_corruptedData(self):
+        message = WrapperMessage(
+            request_for_fast_response=RequestForFastResponse()
+        )
+
+        data = _VarintBytes(message.ByteSize()) + message.SerializeToString()
+
+        arr = bytearray(data)
+        arr[0] -= 1
+        data = bytes(arr)
+
+        message, bytesConsumed = parseDelimited(data, WrapperMessage)
+        self.assertIsNone(message)
+        self.assertEqual(bytesConsumed, len(data) - 1)
